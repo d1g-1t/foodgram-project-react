cbcec6e8fabb6af60d85dee641453ebc140b5db1
Pavel Okhrim <133741770+d1g-1t@users.noreply.github.com>
2024-01-29 12:03:36 +0300

    Обновлены пути статики в nginx и docker-compose.production.yml.

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'backend_static'
+STATIC_ROOT = os.path.join(BASE_DIR / 'static/')
 
 MEDIA_URL = '/media/'
 
@@ -135,9 +135,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
-    'DEFAULT_PAGINATION_CLASS': (
-        'rest_framework.pagination.PageNumberPagination',
-    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6,
 }
 
